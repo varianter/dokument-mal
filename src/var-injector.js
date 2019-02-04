a3bc36562ec9ca8c1633a0b7f2d5acbe92afe10e
Mikael Brevik <mikaelbre@gmail.com>
2019-02-04 21:54:05 +0100

    Adds url box generation from query params

--- src/var-injector.js
+++ src/var-injector.js
@@ -16,25 +16,43 @@ const modifiers = {
   }
 };
 
-class Field extends HTMLElement {
-  constructor() {
-    super();
-  }
-}
-customElements.define("v-field", Field);
-
 window.addEventListener("load", function() {
   const main = sel("main");
   const fields = getFields(main);
-  const form = constructForm(fields);
+  prefillCustomElementsWithQuery(fields);
+
+  const form = constructForm(fields, updateUrlBox);
   document.body.insertBefore(form, main);
+
+  updateUrlBox();
 });
 
-function getFields(selector) {
+function getFields() {
   return Array.from(sela("v-field"));
+}
+function getInputs() {
+  return Array.from(sela("input")).filter(i => i.id.indexOf("input-") === 0);
 }
 function getFieldsOfType(selector) {
   return Array.from(sela(`v-field[title='${selector}']`));
+}
+function getObjectFromFields() {
+  let obj = {};
+  for (let field of getInputs()) {
+    obj[field.id] = field.value;
+  }
+  return obj;
+}
+
+function prefillCustomElementsWithQuery(fields) {
+  const defaultValues = queryToObject();
+
+  fields.forEach(function updateDefaultValues(field) {
+    const slug = "input-" + slugify(field.getAttribute("title"));
+    const data = defaultValues[slug];
+    if (!slug || !data) return;
+    field.textContent = defaultValues[slug];
+  });
 }
 
 function updateValue(field, value) {
@@ -45,17 +63,48 @@ function updateValue(field, value) {
   });
 }
 
-function constructForm(fields) {
+function updateUrlBox() {
+  const updateBox = document.querySelector("#urlbox");
+  const data = getObjectFromFields();
+  const url = objectToUrl(data);
+  updateBox.value = url;
+}
+
+function constructForm(fields, cb) {
   const form = ce("form");
   const ul = ce("ul");
 
+  const updateDecoration = (...args) => {
+    cb();
+    updateValue(...args);
+  };
+
   unique(fields)
-    .map(createConstructInputLi(updateValue))
+    .map(createConstructInputLi(updateDecoration))
     .forEach(ul.appendChild.bind(ul));
 
+  const urlBox = createUrlBox();
+  ul.appendChild(urlBox);
   form.appendChild(ul);
-
   return form;
+}
+
+function createUrlBox() {
+  const label = ce("label");
+  label.setAttribute("for", "urlbox");
+  label.textContent = "URL";
+
+  const input = ce("input");
+  input.name = "urlbox";
+  input.id = "urlbox";
+  input.type = "text";
+  input.value = "";
+  input.readOnly = true;
+
+  const li = ce("li");
+  li.appendChild(label);
+  li.appendChild(input);
+  return li;
 }
 
 function createConstructInputLi(cb) {
@@ -108,4 +157,26 @@ function slugify(str) {
     .trim()
     .replace(/&/g, "-")
     .replace(/[\s\W-]+/g, "-");
+}
+
+function queryToObject() {
+  const params = new URLSearchParams(document.location.search);
+  let obj = {};
+  for (let [key, val] of params) {
+    obj[key] = val;
+  }
+
+  return obj;
+}
+
+function objectToUrl(obj) {
+  const params = new URLSearchParams();
+  for (let key of Object.keys(obj)) {
+    if (obj[key]) params.set(key, obj[key]);
+  }
+  return getUrl() + "?" + params.toString();
+}
+
+function getUrl() {
+  return document.location.href.split("?")[0];
 }
